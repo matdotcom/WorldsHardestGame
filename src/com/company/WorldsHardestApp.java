f0cac14e18741593e4623851d64c32694800e54f
Mathias Bruun Hemmingsen <31951525+tejsfrank@users.noreply.github.com>
2018-02-06 05:49:54 +0100

    Fået Firkanten til at bevæge sig på WASD tasterne. GREAT SUCCESS

--- src/com/company/WorldsHardestApp.java
+++ src/com/company/WorldsHardestApp.java
@@ -1,14 +1,27 @@
 package com.company;
 
 import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.settings.GameSettings;
+import common.PlayerControl;
+import javafx.scene.input.KeyCode;
+import javafx.scene.shape.Rectangle;
 
 public class WorldsHardestApp extends GameApplication {
 
-  
-
+    // laver en player. Vi definerer vores ting i spillet i form af enums.
+private enum Type {
+    PLAYER
+}
+private PlayerControl playerControl;
+private Entity player;
     @Override
     protected void initSettings(GameSettings gameSettings) {
+        // basale gamessetings. Styrer størrelsen af vores program. Og om der skal være en menu osv.
+        // For nu slår vi alt fra.
         gameSettings.setWidth(1200);
         gameSettings.setHeight(800);
         gameSettings.setTitle("Worlds Hardest Game!");
@@ -18,8 +31,45 @@ public class WorldsHardestApp extends GameApplication {
         gameSettings.setIntroEnabled(false);
         gameSettings.setMenuEnabled(false);
     }
-    protected void initInput(){
 
+    @Override
+    protected void initGame() {
+        playerControl = new PlayerControl();
+        // Spawner vores spiller, vi definerer hvor den spawner, samt hvor stor den skal være.
+player = Entities.builder().type(Type.PLAYER).at(25,100).viewFromNode(new Rectangle(25,25)).with(playerControl).buildAndAttach(getGameWorld());
+    }
+
+    @Override
+    protected void initInput() {
+        Input input = getInput();
+
+        input.addAction(new UserAction("Move Left") {
+            @Override
+            protected void onAction() {
+                playerControl.left();
+            }
+        }, KeyCode.A);
+
+        input.addAction(new UserAction("Move Right") {
+            @Override
+            protected void onAction() {
+                playerControl.right();
+            }
+        }, KeyCode.D);
+
+        input.addAction(new UserAction("Move Up") {
+            @Override
+            protected void onAction() {
+                playerControl.up();
+            }
+        }, KeyCode.W);
+
+        input.addAction(new UserAction("Move Down") {
+            @Override
+            protected void onAction() {
+                playerControl.down();
+            }
+        }, KeyCode.S);
     }
 
     public static void main(String[] args) {
---
+++ src/common/PlayerControl.java
@@ -0,0 +1,43 @@
+package common;
+
+import com.almasb.fxgl.entity.Control;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.component.PositionComponent;
+import com.almasb.fxgl.entity.component.Required;
+import com.almasb.fxgl.entity.Control;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.component.PositionComponent;
+import com.almasb.fxgl.entity.component.Required;
+
+    /**
+     * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
+     */
+    @Required(PositionComponent.class)
+    public class PlayerControl extends Control {
+
+        private PositionComponent position;
+
+        private double speed = 0;
+
+        @Override
+        public void onUpdate(Entity entity, double tpf) {
+            speed = tpf * 60;
+        }
+
+        public void up() {
+            position.translateY(-5 * speed);
+        }
+
+        public void down() {
+            position.translateY(5 * speed);
+        }
+
+        public void left() {
+            position.translateX(-5 * speed);
+        }
+
+        public void right() {
+            position.translateX(5 * speed);
+        }
+    }
+
