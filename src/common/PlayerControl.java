37b7b4fc7e3d327ee7a5151564dd3f4cb478a089
Mathias Bruun Hemmingsen <31951525+tejsfrank@users.noreply.github.com>
2018-02-10 14:21:49 +0100

    Tilføjet musik og lyd når man dør!

--- src/common/PlayerControl.java
+++ src/common/PlayerControl.java
@@ -23,19 +23,19 @@ import com.almasb.fxgl.entity.component.Required;
         }
 
         public void up() {
-            position.translateY(-1.5 * speed);
+            position.translateY(-1.1 * speed);
         }
 
         public void down() {
-            position.translateY(1.5 * speed);
+            position.translateY(1.1 * speed);
         }
 
         public void left() {
-            position.translateX(-1.5 * speed);
+            position.translateX(-1.1 * speed);
         }
 
         public void right() {
-            position.translateX(1.5 * speed);
+            position.translateX(1.1 * speed);
         }
     }
 
